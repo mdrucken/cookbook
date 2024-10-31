c53c16c87094540f978fcf9c4070b1ad30ca6659
Marc Druckenmueller <marc.druckenmueller@gmail.com>
2024-10-31 15:48:44 +0000

    fix xml parsing

--- tool/validate.py
+++ tool/validate.py
@@ -1,7 +1,7 @@
 import os
 from lxml import etree
 
-with open("src/recipeml.xsd", 'r') as file:
+with open("src/recipeml.xsd", 'rb') as file:
     schema_content = file.read()
     xsd_doc = etree.XML(schema_content)
     schema = etree.XMLSchema(xsd_doc)
@@ -10,14 +10,17 @@ with open("src/recipeml.xsd", 'r') as file:
 FOLDER = "src"
 for filename in os.listdir(FOLDER):
     file_path = os.path.join(FOLDER, filename)
-    with open(file_path, 'r') as file:
+    with open(file_path, 'rb') as file:
         xml_content = file.read()
         try:
             xml_doc = etree.XML(xml_content)
         except ValueError as ve:
             print(f"Failed to read {file_path}\n{ve}")
             continue
-        valid = schema.validate(xml_doc)
+        try:
+            valid = schema.validate(xml_doc)
+        except Exception as e:
+            print(f"Error when validating {file_path}\n{e}")
         if not valid:
             print(f"XML file {file_path} is NOT valid against the provided XSD schema.")
             # Print validation errors
